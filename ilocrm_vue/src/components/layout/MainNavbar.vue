8673ce130fe01227ce50770fb29de0e80d1feb33
sparklefoa1 <caizixuan21@163.com>
2024-06-28 19:06:47 +0100

    add navbar component

---
+++ ilocrm_vue/src/components/layout/MainNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar is-dark">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <strong>ILO CRM</strong>
+            </router-link>
+        </div>
+    </nav>
+
+    <div class="navbar-menu">
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="button">
+                    <router-link to="/log-in" class="button is-light">Log in</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
