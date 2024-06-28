c62afb498cfdf3ce407e5b34bc7b02b825f19d10
sparklefoa1 <caizixuan21@163.com>
2024-06-28 17:16:03 +0100

    basic set

---
+++ ilocrm_vue/src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+// 将 axios 添加到 Vue 实例的全局属性中
+app.config.globalProperties.$axios = axios;
+
+// 挂载 Vue 应用程序到 HTML 元素上
+app.mount('#app');
