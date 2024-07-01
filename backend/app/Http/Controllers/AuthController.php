60eb667a5751c26840379fcd306fe44114307061
ClydeZH23 <clydezh23@gmail.com>
2024-07-01 04:29:29 +0100

    authcontroller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -6,18 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request) {
-        $request->validate()
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->creatToken('LoginToken')->plainTextToken;
 
+            return response()->json(['user' => $user, 'token' => $token], 200);
         }
+
+        return response()->json(['error' => 'Unauthorized',],401);
     }
 
     public function logout(Request $request) {
-
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Successfully logged out']);
     }
 }
