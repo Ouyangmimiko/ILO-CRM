fb3777a6c852cbbd28ad8a87c9c3d2d9fa93cc4f
ClydeZH23 <clydezh23@gmail.com>
2024-07-05 02:01:31 +0100

    setup cors

--- backend/README.md
+++ backend/README.md
@@ -7,7 +7,7 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
-Cross domain connection
+Cross-Origin Resource Sharing for cross origin request between frontend and backend
     CORS
     config/cors.php
 
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'], // ['GET', 'POST', 'UPDATE', ...]
 
-    'allowed_origins' => ['*'], // ['http://localhost:5173'] for frontend server
+    'allowed_origins' => ['http://localhost:5173'], // ['http://localhost:5173'] for frontend server
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
